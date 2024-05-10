aa71f8953b6f930bb1fac958a4c869777315d5c3
samandar <samandar200527@gmail.com>
2024-05-10 15:52:34 +0200

    retrieving file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Clouds
 import os, sqlite3
 from utils import generate_6_digit_id
-
+import time
 
 class CloudStorage:
     def __init__(self, file_path, type) -> None:
@@ -9,6 +9,7 @@ class CloudStorage:
         self.size=os.path.getsize(self.get_file_path())
         self.type=type
         self.file_id=generate_6_digit_id()
+        # self.curr_time=time.time()
         
     def get_file_path(self):
         return self.__file_path
@@ -26,9 +27,34 @@ class CloudStorage:
             return None
     
     def store_file(self):
-        pass
+        try:
+            connection=sqlite3.connect('storage.db')
+            cursor=connection.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS Files (id INTEGER PRIMARY KEY, file_id INTEGER content BLOB, timestamp INTEGER)''')
+            timestamp = int(time.time())  
+            cursor.execute('''INSERT INTO Files (content, timestamp) VALUES (?, ?, ?)''', (self.file_id, self.read_file(), timestamp))
+            connection.commit()
+            print("Stored...")
+        except sqlite3.Error as e:
+            print(f"Error: SQLite error - {e}")
+        finally:
+            connection.close()
     
     def retrieve_file(self, file_id):
+        try:
+            connection = sqlite3.connect('storage.db')
+            cursor = connection.cursor()
+            cursor.execute('''SELECT content FROM Files WHERE file_id = ?''', (file_id,))
+            file_content = cursor.fetchone()[0]
+            connection.close()
+            return file_content
+        except sqlite3.Error as er:
+            print(er)
+            return None
+        finally:
+            connection.close()
+    
+    def vanish_file(self, file_id):
         pass
 
 class SendAnywhere(CloudStorage):
