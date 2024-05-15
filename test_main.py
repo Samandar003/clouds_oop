65a15ccf466841feefba98935b2ba3490f0309b3
samandar <samandar200527@gmail.com>
2024-05-15 15:58:07 +0200

    test store and retrieve

--- test_main.py
+++ test_main.py
@@ -11,15 +11,25 @@ class TestCloudStorage(unittest.TestCase):
         self.assertTrue(CloudStorage.file_exist("main.py"))
         
     def test_give_filename(self):
-        self.assertEqual("/home/samandar/samandar/cloud_project/main.py", "main.py")
+        self.assertEqual(self.obj1.give_file_name("/home/samandar/samandar/cloud_project/main.py"), "main.py")
     
     def test_check_filesize(self):
-        self.assertIsNotNone(self.obj("main.py"))
+        self.assertIsNotNone(self.obj1.check_file_size("main.py"))
         
     def test_6digit(self):
-        self.assertGreater(CloudStorage.generate_6_digit_id(), 99999)
+        self.assertGreater(int(CloudStorage.generate_6_digit_id()), 99999)
 
     def test_readfile(self):
-        self.assertEqual(self.obj1.read_file("something.html"), "<!DOCTYPE html>")
+        self.assertIsNot(self.obj1.read_file("something.html"), "")
         
+    def test_store_file(self):
+        self.assertGreater(self.obj1.store_file("something.html"), 0)
+        
+    def test_retrieve(self):
+        self.assertEqual(self.obj1.retrieve_file(214689), "donwloaded..")
+        
+    
+    
+if __name__ == '__main__':
+    unittest.main()       
     
